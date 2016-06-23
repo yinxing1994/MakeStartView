5e90a3f3b8ea2e2d20b01bcd039bf304de73c425
yinxing1994 <yinxing@yinxingdeMacBook-Pro.local>
2016-06-23 17:20:49 +0800

    星星评分
    简单好用

--- MakeStartView/ViewController.m
+++ MakeStartView/ViewController.m
@@ -7,16 +7,27 @@
 //
 
 #import "ViewController.h"
-
+#import "CWStarRateView.h"
 @interface ViewController ()
-
+{
+    CWStarRateView *bigStarRateView;
+    
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    bigStarRateView=[[CWStarRateView alloc]initWithFrame: CGRectMake(0, 0, self.view.bounds.size.width, 200) numberOfStars:5];
+    
+    bigStarRateView.scorePercent = 0.2;
+    bigStarRateView.allowIncompleteStar = NO;//是否允许评价半颗星
+    bigStarRateView.hasAnimation = YES;
+    [self.view addSubview:bigStarRateView];
 }
 
 - (void)didReceiveMemoryWarning {
